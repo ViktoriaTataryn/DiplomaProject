c2e29dee26658a094b15127878ad5cdb812d9dc7
ViktoriaTataryn <viktoriatatarin252@gmail.com>
2026-01-22 10:46:11 +0200

    add .gitignore

--- diplomaProject/diplomaProject/obj/Debug/net8.0/diplomaProject.AssemblyInfo.cs
+++ diplomaProject/diplomaProject/obj/Debug/net8.0/diplomaProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diplomaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba1ade3391f2f7ff7a514a50cf0ad1ff29bd499")]
 [assembly: System.Reflection.AssemblyProductAttribute("diplomaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diplomaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
