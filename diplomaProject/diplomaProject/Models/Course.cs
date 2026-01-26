32be897bb9cb534f2ece7a233cbc4cd896dc28e3
KickBeLLchic <mhelkat44@gmail.com>
2026-01-26 23:31:27 +0200

    Save models

---
+++ diplomaProject/diplomaProject/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace diplomaProject.Models
+{
+    public class Course
+    {
+        //у каждого курса должен быть уникальный номер (ID)
+        [Key]
+        public int Id { get; set; }
+
+        // Название курса
+        [Required] //название обязательно должно быть
+        public string Title { get; set; }
+        // Описание курса
+        public string Description { get; set; }
+
+        // Дата создания (чтобы знать, когда курс добавили)
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ diplomaProject/diplomaProject/Models/UserProgress.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace diplomaProject.Models
+{
+    // класс для отслеживания успехов студента в обучении
+    public class UserProgress
+    {
+        //ункальный ID записи о прогрессе
+        [Key]
+        public int Id { get; set; }
+
+        //ссылка на ID пользователя (связь с таблицей User)
+        [Required]
+        [Display(Name = "User ID")]
+        public int UserId { get; set; }
+
+        //ссылка на ID курса (связь с таблицей Course)
+        [Required]
+        [Display(Name = "Course ID")]
+        public int CourseId { get; set; }
+
+        // наа сколько процйентов пройден курс (от 0 до 100)
+        [Display(Name = "Completion Percentage")]
+        public int CompletionPercentage { get; set; } = 0;
+
+        // флаг: закончил ли студент курс полностью?
+        [Display(Name = "Is Completed")]
+        public bool IsCompleted { get; set; } = false;
+
+        // дата и время последнего захода в материалы курса
+        [Display(Name = "Last Activity Date")]
+        public DateTime LastActivity { get; set; } = DateTime.Now;
+    }
+}
