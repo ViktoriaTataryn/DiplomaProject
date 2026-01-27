2ccc8a0020f1670072878b013d0d14ed1ead186f
ViktoriaTataryn <viktoriatatarin252@gmail.com>
2026-01-27 21:47:10 +0200

    Add DbContext

--- diplomaProject/diplomaProject/Models/CourseRegistration.cs
+++ diplomaProject/diplomaProject/Models/CourseRegistration.cs
@@ -4,9 +4,7 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        //public User User { get; set; }
         public int CourseId { get; set; }
-        // public Course Course { get; set; }
         public DateTime RegisterAt { get; set; } = DateTime.UtcNow;
     }
 }
--- diplomaProject/diplomaProject/Models/Review.cs
+++ diplomaProject/diplomaProject/Models/Review.cs
@@ -7,7 +7,7 @@ namespace diplomaProject.Models
         public int Id { get; set; }
         public string Content { get; set; }
         public int UserId { get; set; }
-        //public User User { get; set; }
+        public User User { get; set; }
         public int Rating { get; set; }
     }
 }
