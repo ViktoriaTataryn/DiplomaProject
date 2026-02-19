380b785b3fe48fd105e027d753eab2b888655143
Andry <mhelkat44@gmail.com>
2026-02-19 22:18:11 +0200

    Add Homework and Resource models

---
+++ diplomaProject/diplomaProject/Models/Homework.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace diplomaProject.Models
+{
+    public class Homework
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Task Description")]
+        public string Description { get; set; } // Описание того, что нужно сделать
+
+        [Display(Name = "Due Date")]
+        public DateTime? DueDate { get; set; } // Крайний срок сдачи (дедлайн)
+
+        // Связь с уроком: к какому уроку относится эта домашка
+        public int LessonId { get; set; }
+        public Lesson Lesson { get; set; }
+
+        // Список ответов, которые пришлют студенты
+        public ICollection<HomeworkSubmission> Submissions { get; set; } = new List<HomeworkSubmission>();
+    }
+}
--- diplomaProject/diplomaProject/Models/Lesson.cs
+++ diplomaProject/diplomaProject/Models/Lesson.cs
@@ -1,15 +1,28 @@
-﻿namespace diplomaProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace diplomaProject.Models
 {
     public class Lesson
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Lesson Title")]
         public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Content")]
         public string Content { get; set; }
-        //public string? VideoUrl { get; set; }
+
+        [Display(Name = "Video URL")]
+        public string? VideoUrl { get; set; } // Ссылка на видео, если будет нужно
+
+        // Связь с модулем к которому относится урок
         public Module Module { get; set; }
         public int ModuleId { get; set; }
-        public ICollection<Resource>? Resources { get; set; }
-      
 
+        // Список всех файлов (картинок, методичек), привязанных к уроку
+        [Display(Name = "Resources")]
+        public ICollection<Resource> Resources { get; set; } = new List<Resource>();
     }
 }
